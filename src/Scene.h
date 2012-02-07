aea70aa9bcce953ea7443d8721ea7fc90416d344
megadrive <a.simeon@gmail.com>
2012-02-07 17:21:19 +1100

    Updated src/Scene.h: Added Create/Destroy classes.

--- src/Scene.h
+++ src/Scene.h
@@ -1,4 +1,3 @@
-
 #ifndef __SCENE_H
 #define __SCENE_H
 
@@ -7,11 +6,16 @@
  * @brief	Describes the base of a Scene. Any other scenes need to be derived off this.
  */
 
+#include "Util.h"
+
 class Scene
 {
 public:
 	Scene();
 	virtual ~Scene();
+	
+	virtual RR_RESULT Create() = 0;
+	virtual RR_RESULT Destroy() = 0;
 
 	virtual void Update(float a_fDeltaTime) = 0;
 	virtual void Render() = 0;
