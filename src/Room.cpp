d903b3a7007a8cf3c38ed5df5deccec1641e3a43
Andrew Simeon <a.simeon@gmail.com>
2012-02-08 02:21:12 -0800

    Defined AddObject() and RemoveAllObjects().

--- src/Room.cpp
+++ src/Room.cpp
@@ -19,15 +19,38 @@ Room::~Room()
  */
 RR_RESULT Room::AddObject(Object* a_pObject)
 {
-	return RR_RESULT_OK;
+	if( a_pObject )
+	{
+		m_vObjects.push_back(a_pObject);
+		return RR_RESULT_OK;
+	}
+
+	return RR_RESULT_ERROR;
 }
 
 /**
  * RemoveAllObjects
  * @brief Remove all objects from the room.
  */
-RR_RESULT Room::RemoveAllObjects()
+RR_RESULT Room::RemoveAllObjects( bool a_bDestroy = false )
 {
+	if( m_vObjects.size() )
+	{
+		for( ObjectVector::iterator iter = m_vObjects.end(); iter != m_vObjects.start(); --iter )
+		{
+			if( a_bDestroy )
+			{
+				Object* pObject = *iter;
+				pObject->Destroy();
+				m_vObjects.pop_back();
+			}
+		}
+	}
+	else
+	{
+		return RR_RESULT_ERROR;
+	}
+
         return RR_RESULT_OK;
 }
 
