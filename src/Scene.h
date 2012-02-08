372a36f218f05578faee1df32bf9e194359da6b9
Andrew <a.simeon+thegithub@gmail.com>
2012-02-09 01:11:56 +1100

    Modified Scene to have its constructor/destructor defined to avoid errors. Added Run(). Modified Update to return a StateManager::STATE.

--- src/Scene.h
+++ src/Scene.h
@@ -8,18 +8,20 @@
  */
 
 #include "Util.h"
+enum SceneManager::STATE;
 
 class Scene
 {
 public:
-	Scene();
-	virtual ~Scene();
+	Scene(){}
+	virtual ~Scene(){}
 	
 	virtual RR_RESULT Create() = 0;
 	virtual RR_RESULT Destroy() = 0;
 
-	virtual void Update(float a_fDeltaTime) = 0;
+	virtual SceneManager::STATE Update(float a_fDeltaTime) = 0;
 	virtual void Render() = 0;
+	virtual SceneManager::STATE Run(float a_fDeltaTime) = 0;
 };
 
 #endif
