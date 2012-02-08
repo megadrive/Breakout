3c5ec35408c7e0c03ebc13efdd95d0ce8a3572dc
Andrew Simeon <andrew@wandleby.(none)>
2012-02-08 14:46:02 +1100

    Added Create() and Destroy() functions. Added Util.h include.

--- src/MenuScene.cpp
+++ src/MenuScene.cpp
@@ -11,11 +11,40 @@ MenuScene::~MenuScene()
 
 }
 
+/**
+ * Create
+ * @brief Fires when an object is created. Initialise object-specific variables in here.
+ */
+RR_RESULT MenuScene::Create()
+{
+	return RR_RESULT_OK;
+}
+
+/**
+ * Destroy
+ * @brief Fires when an object is destroyed. Uninitialise anything you did in Create.
+ */
+RR_RESULT MenuScene::Destroy()
+{
+	return RR_RESULT_OK;
+}
+
+/**
+ * Update
+ * @brief	Updates this object based on the Delta time.
+ * @param	float	a_fDeltaTime
+ */
 void MenuScene::Update( float a_fDeltaTime )
 {
 
 }
 
+/**
+ * Render
+ * @brief Renders this object to the buffer.
+ * TODO: Add deferred rendering by adding each object to a queue.
+ * TODO: Possibly define this in Object then only override if the Object requires a custom Render.
+ */
 void MenuScene::Render()
 {
 
--- src/MenuScene.h
+++ src/MenuScene.h
@@ -7,12 +7,17 @@
  * @brief	
  */
 
+#include "Util.h"
+
 class MenuScene
 {
 public:
 	MenuScene();
 	virtual ~MenuScene();
 
+	virtual RR_RESULT Create();
+	virtual RR_RESULT Destroy();
+	
 	virtual void Update(float a_fDeltaTime);
 	virtual void Render();
 };
